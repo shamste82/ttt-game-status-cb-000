8b219b92006e0643572b0f36dcc8dd3e5e0e90a5
Steffen Hammelow-Berg <shammelow@gmail.com>
2018-01-13 02:44:59 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -64,7 +64,7 @@ end
 
 def winner(board)
   combination = won?(board)
-  print comination
+  print combination
   if board[combination[0]] == "X" && board[combination[1]] == "X" && board[combination[2]] == "X"
     return "X"
   elsif board[combination[0]] == "O" && board[combination[1]] == "O" && board[combination[2]] == "O"
