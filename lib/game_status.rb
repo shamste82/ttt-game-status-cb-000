b177bfcea1b07b8a422fd2227f8d573145691333
Steffen Hammelow-Berg <shammelow@gmail.com>
2018-01-13 02:34:12 +0000

    automatically backed up by learn

--- lib/game_status.rb
+++ lib/game_status.rb
@@ -64,9 +64,9 @@ end
 
 def winner(board)
   combination = won?(board)
-  if board[combination[0]] == "X"
+  if board[combination[0]] == "X" && board[combination[1]] == "X" && board[combination[2]] == "X"
     return "X"
-  elsif board[combination[0]] == "O"
+  elsif board[combination[0]] == "O" && board[combination[1]] == "O" && board[combination[2]] == "O"
     return "O"
   end
 end
